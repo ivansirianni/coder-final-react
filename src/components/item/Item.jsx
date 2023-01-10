02821f6cbdc11620ff67f85e9b6cf7b2ac88c1f4
Ivan Sirianni <ivansirianni@gmail.com>
2023-01-09 22:18:52 -0300

    final

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -33,15 +33,7 @@ const Item = ({prod}) => {
       </Card>
 
 
-            // <div className=" border-light mb-3 cardProducto">
-            //                 <img src={prod.url} className="card-img-top cardImg" alt="cardimg" />
-            //                 <div className="card-body">
-            //                     <h5 className="card-title">{prod.name}</h5>
-            //                     <p className="card-text">{prod.category}</p>
-            //                     <p className="card-text">u$D {new Intl.NumberFormat('de-DE').format(prod.price)}</p>
-            //                 <Link  to={`/product/${prod.id}`}> <button className='btn-a '>Buy Now</button></Link>
-            //             </div>
-            // </div>
+            
     );
 }
 
