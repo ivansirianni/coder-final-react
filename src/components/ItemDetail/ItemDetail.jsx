5dd1130eb200f16317cc1c4eb87b22264a536ea6
Ivan Sirianni <ivansirianni@gmail.com>
2023-01-10 10:53:26 -0300

    refresh

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -20,7 +20,7 @@ const ItemDetail = ({producto}) => {
             <div className="col-md-8">
                 <div className="card-body">
                     <h5 className='card-title'>{producto.name}</h5>
-                    <p className='card-text'>Price: {producto.details}</p>                    
+                    <p className='card-text'>Detail: {producto.details}</p>                    
                     <p className='card-text'>Price: u$D{producto.price}</p>
                     <p className='card-text'>Stock: {producto.stock} Items</p>
                     <ItemCount stock = {producto.stock} onAdd={onAdd}/><br />
